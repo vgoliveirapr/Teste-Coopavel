a3ea494854a8fb98b84690926bbc2d20ed32eba4
Victor Gabriel de Oliveira <gabvictoroli@gmail.com>
2020-05-05 16:17:20 -0300

    Atualizado alguns parametros, atualizada a função Pegou Ticket e criação de um menu

--- Teste Coopavel/Entities/Associados.cs
+++ Teste Coopavel/Entities/Associados.cs
@@ -6,5 +6,36 @@ namespace Teste_Coopavel.Entities
 {
     class Associados
     {
+
+        public static string Nome { get; private set; }
+        public static int Tickets { get; private set; }
+
+
+        public Associados(){
+        }
+
+        public Associados(string nome, int tickets)
+        {
+            Nome = nome;
+            Tickets = tickets;
+        }
+
+        public static void PegouTicket(string nome) { 
+        
+            
+            if(Nome == nome && Tickets <= 0)
+            {
+                Console.WriteLine("O associado informado ainda não pegou os seus tickets!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("O associado informado já retirou seus tickets!");
+                Console.WriteLine("Retirou: " + Tickets);
+                Console.WriteLine();
+            }
+        
+        }
+
     }
 }
--- Teste Coopavel/Program.cs
+++ Teste Coopavel/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Teste_Coopavel.Entities;
+using System.Collections.Generic;
 
 namespace Teste_Coopavel
 {
@@ -6,7 +8,64 @@ namespace Teste_Coopavel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao = 0;
+
+            while (opcao != 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine("             Bem-vindo ao sistema de tickets de almoço - Show Rural");
+                Console.WriteLine();
+
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("1 - Cadastrar a retirada do ticket do associado");
+                Console.WriteLine("2 - Verificar a retirada do ticket do associado");
+                Console.WriteLine("3 - Saldo final do evento");
+                Console.WriteLine("4 - Sair");
+                Console.Write("Opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+
+                if (opcao == 1)
+                {
+                    Console.WriteLine();
+                    Console.Write("Informe o nome do associado: ");
+                    string Nome = Console.ReadLine();
+
+                    Console.Write("Informe a quantidade de tickets: ");
+                    int Tickets = int.Parse(Console.ReadLine());
+
+                    List<Associados> associados = new List<Associados>();
+
+                    associados.Add(new Associados(Nome, Tickets));
+
+                    Console.Clear();
+
+                }
+                else if (opcao == 2)
+                {
+                    Console.WriteLine();
+                    Console.Write("Informe o nome do associado: ");
+                    string Nome = Console.ReadLine();
+
+                    Associados.PegouTicket(Nome);
+                    Console.WriteLine("Pressione qualquer tecla para voltar!!");
+                    Console.ReadLine();
+
+                    Console.Clear();
+
+                }
+                else if (opcao == 3)
+                {
+
+                    Console.Clear();
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Obrigado por utilizar!!");
+                }
+            }
         }
     }
 }
