c7561d716bf5f45398892bcc961a9c34e680d200
Victor Gabriel de Oliveira <gabvictoroli@gmail.com>
2020-05-06 10:46:43 -0300

    Finalizado o problema apresentado no teste, programa funciona

--- Teste Coopavel/Entities/Associados.cs
+++ Teste Coopavel/Entities/Associados.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Teste_Coopavel.Entities
 {
     class Associados
     {
 
-        public static string Nome { get; private set; }
-        public static int Tickets { get; private set; }
+        public string Nome { get; private set; }
+        public  int Tickets { get; private set; }
+        public int Codigo { get; private set; }
+        public bool JaRetirou { get; private set; }
 
 
-        public Associados(){
+        public Associados()
+        {
         }
 
-        public Associados(string nome, int tickets)
+        public Associados(string nome, int tickets, int codigo)
         {
             Nome = nome;
             Tickets = tickets;
+            Codigo = codigo;
+            JaRetirou = true;
         }
 
-        public static void PegouTicket(string nome) { 
-        
-            
-            if(Nome == nome && Tickets <= 0)
-            {
-                Console.WriteLine("O associado informado ainda não pegou os seus tickets!");
-                Console.WriteLine();
-            }
-            else
-            {
-                Console.WriteLine("O associado informado já retirou seus tickets!");
-                Console.WriteLine("Retirou: " + Tickets);
-                Console.WriteLine();
-            }
-        
+        public Associados(string nome, int codigo) {
+
+            Nome = nome;
+            Tickets = 0;
+            Codigo = codigo;
+            JaRetirou = false;
+
+        }
+        public override string ToString()
+        {
+            return Codigo + ", " + Nome + ", " + Tickets;
         }
 
     }
--- Teste Coopavel/Program.cs
+++ Teste Coopavel/Program.cs
@@ -6,66 +6,124 @@ namespace Teste_Coopavel
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int opcao = 0;
+            List<Associados> lista = new List<Associados>();
 
-            while (opcao != 4)
+            while (opcao != 3)
             {
-                Console.WriteLine();
-                Console.WriteLine("             Bem-vindo ao sistema de tickets de almoço - Show Rural");
-                Console.WriteLine();
-
-                Console.WriteLine("O que deseja fazer?");
-                Console.WriteLine("1 - Cadastrar a retirada do ticket do associado");
-                Console.WriteLine("2 - Verificar a retirada do ticket do associado");
-                Console.WriteLine("3 - Saldo final do evento");
-                Console.WriteLine("4 - Sair");
-                Console.Write("Opção: ");
-                opcao = int.Parse(Console.ReadLine());
-
-
-                if (opcao == 1)
+                try
                 {
+
                     Console.WriteLine();
-                    Console.Write("Informe o nome do associado: ");
-                    string Nome = Console.ReadLine();
-
-                    Console.Write("Informe a quantidade de tickets: ");
-                    int Tickets = int.Parse(Console.ReadLine());
-
-                    List<Associados> associados = new List<Associados>();
-
-                    associados.Add(new Associados(Nome, Tickets));
-
-                    Console.Clear();
-
-                }
-                else if (opcao == 2)
-                {
+                    Console.WriteLine("             Bem-vindo ao sistema de tickets de almoço - Show Rural");
                     Console.WriteLine();
-                    Console.Write("Informe o nome do associado: ");
-                    string Nome = Console.ReadLine();
 
-                    Associados.PegouTicket(Nome);
-                    Console.WriteLine("Pressione qualquer tecla para voltar!!");
-                    Console.ReadLine();
+                    Console.WriteLine("O que deseja fazer?");
+                    Console.WriteLine("1 - Cadastrar a retirada do ticket do associado");
+                    Console.WriteLine("2 - Verificar a retirada do ticket do associado");
+                    Console.WriteLine("3 - Saldo final do evento e Saída do programa");
+                    Console.Write("Opção: ");
+                    opcao = int.Parse(Console.ReadLine());
 
-                    Console.Clear();
 
+                    switch (opcao)
+                    {
+
+                        case 1:
+
+                            Console.WriteLine();
+
+                            Console.Write("Informe o codigo de cadastro do associado: ");
+                            int Codigo = int.Parse(Console.ReadLine());
+
+                            Console.Write("Informe o nome do associado: ");
+                            string Nome = Console.ReadLine();
+
+                            Console.WriteLine("Ja retirou os tickets? (S/N)");
+                            char Resposta = char.Parse(Console.ReadLine());
+
+                            if (Resposta == 'S' || Resposta == 's')
+                            {
+                                Console.Write("Informe a quantidade de tickets: ");
+                                int Tickets = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                Associados cadastroAssociado = new Associados(Nome, Tickets, Codigo);
+                                lista.Add(cadastroAssociado);
+                                Console.WriteLine("     Cadastrado com sucesso!!");
+                                
+                            }
+                            else if (Resposta == 'N' || Resposta == 'n')
+                            {
+                                Console.Clear();
+                                Associados cadastroAssociado = new Associados(Nome, Codigo);
+                                lista.Add(cadastroAssociado);
+                                Console.WriteLine("     Cadastrado com sucesso!!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Opção inválida!!!");
+                            }
+
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine();
+                            Console.Write("Informe o nome do associado: ");
+                            string pesquisaNome = Console.ReadLine();
+
+                            foreach (Associados consultado in lista)
+                            {
+                                if (consultado.Nome == pesquisaNome && consultado.Tickets > 0)
+                                {
+                                    Console.WriteLine("O associado informado já retirou seus tickets!");
+                                    Console.WriteLine("Retirou: " + consultado.Tickets);
+                                    Console.WriteLine();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("O associado informado ainda não pegou os seus tickets!");
+                                    Console.WriteLine();
+                                }
+                            }
+
+                            Console.WriteLine("Pressione qualquer tecla para voltar!!");
+                            Console.ReadLine();
+
+                            Console.Clear();
+                            break;
+
+                        default:
+
+                            Console.WriteLine();
+                            Console.WriteLine("Codigo, Nome, Numero de tickets");
+
+                            foreach (Associados saldoFinal in lista)
+                            {
+                                if (saldoFinal.JaRetirou == true)
+                                {
+                                    Console.WriteLine(saldoFinal.ToString());
+                                }
+                            }
+
+                            Console.WriteLine("Pressione qualquer tecla para sair!!");
+                            Console.ReadLine();
+
+                            break;
+
+                    }
                 }
-                else if (opcao == 3)
+                catch (Exception e)
                 {
-
                     Console.Clear();
-
-                }
-                else
-                {
                     Console.WriteLine();
-                    Console.WriteLine("Obrigado por utilizar!!");
+                    Console.WriteLine("Error: " + e.Message);
                 }
             }
+
         }
     }
 }
